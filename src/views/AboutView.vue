0ecfdfc8c6be42250c8f7f865761f6743d187456
InganathiStemela <inganathistemela13@gmail.com>
2024-03-05 09:13:38 +0200

    updates

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <h1>Who am I?</h1>
-    <div class="container">
+    <div class="container-fluid">
       <div class="image">
         <img src="https://i.postimg.cc/8ztnKXhk/IMG-20240112-WA0014.jpg" width="180px" height="180px" alt="My Image">
       </div>
@@ -10,15 +10,13 @@
         </p>
     </div>
   </div>
-  <h2>Hobbies</h2>
-  <div class="container">
+  <h1>Hobbies</h1>
         <p>
          Netball has been a lifelong passion for me since childhood. I fondly remember following my sister to the gym then came back and created my own ball using plastics so that I can have a ball to play with my peers, pretending to be a superstar around my peers. As I grew, my love for the game strengthened. While I never pursued it professionally, netball remains a cherished hobby, offering moments of joy and excitement, serving as an escape from the everyday grind. Whether playing with friends or watching major matches on TV, netball holds a special place in my heart.
         </p>
-      <div class="image2">
+      <div class="image">
         <img src="https://i.postimg.cc/G2CdnSVZ/IMG-20220912-WA0021.jpg" width="200px" height="200px" alt="My Image">
       </div>
-    </div>
   </div>
 </template>
 <style  src="@/assets/CSS/style.css">
